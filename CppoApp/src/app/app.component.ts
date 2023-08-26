0fd3fda5c661b82ebd2fe199bf43ad21bb6c7f6d
barak shuli <ccpoc2023@gmail.com>
2023-08-26 18:40:04 +0300

    new state

--- CppoApp/src/app/app.component.ts
+++ CppoApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
+import { Vehicle, CartService } from './cart/cart.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  cartService =  inject(CartService);
+
   title = 'CppoApp';
+
+  // render list of vehicles
+  vehicles = [
+    {
+      id: 1,
+      name: 'Toyota',
+      price: 10000,
+    },
+    {
+      id: 2,
+      name: 'Honda',
+      price: 20000,
+    },
+    {
+      id: 3,
+      name: 'Ford',
+      price: 30000,
+    },
+    {
+      id: 4,
+      name: 'Chevy',
+      price: 40000,
+    },
+    {
+      id: 5,
+      name: 'Tesla',
+      price: 50000,
+    },
+  ];
+
+
+  addToCart(id: number) {
+    console.log(id);
+    const vehicle = this.vehicles.find((vehicle) => vehicle.id === id);
+    if(vehicle) {
+      this.cartService.addToCart(vehicle);
+    }
+  }
 }
